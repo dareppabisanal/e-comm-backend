b79fc63b5bb664d3098ae9c68c199d9b914317d4
Dareppa Bisanal <insta.dareppa@gmail.com>
2026-02-12 08:55:55 +0530

    Removed sensitive data from files

--- src/utils/file-upload.service.js
+++ src/utils/file-upload.service.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
 
 cloudinary.config({
-    cloud_name: 'dedxptzdo',
-    api_key: '243685385556748',
-    api_secret: '9bIXaKNdwaM2RKDjQN7_tfDU-Pk'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const fileUploadService = async (filePath) => {
